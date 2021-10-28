1ddfc7f398d63eb3003e36d6316c7bb9c89cac47
PhamneyuT <koduapass@gmail.com>
2021-10-28 11:22:15 +0700

    basic format of login

--- DigitalPhotographyManagementSystem/Login/MainWindow.xaml.cs
+++ DigitalPhotographyManagementSystem/Login/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace DigitalPhotographyManagementSystem
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
