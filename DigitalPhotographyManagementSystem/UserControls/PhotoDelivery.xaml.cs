89b64adfc3f9e53ad8c25a0970103d5af861cd25
Thanh Hien Tran <71955948+HinTran013@users.noreply.github.com>
2021-10-30 21:41:39 +0700

    add print photo and photo delivery user controls

---
+++ DigitalPhotographyManagementSystem/UserControls/PhotoDelivery.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DigitalPhotographyManagementSystem.UserControls
+{
+    /// <summary>
+    /// Interaction logic for PhotoDelivery.xaml
+    /// </summary>
+    public class InvoiceDelivery
+    {
+        public int No { get; set; }
+        public string invoiceID { get; set; }
+        public string customerName { get; set; }
+        public string dueDate { get; set; }
+    }
+
+    public partial class PhotoDelivery : UserControl
+    {
+        public PhotoDelivery()
+        {
+            InitializeComponent();
+
+            listInvoice.Items.Add(new InvoiceDelivery()
+            {
+                No = 1,
+                invoiceID = "123",
+                customerName = "Thanh Hien",
+                dueDate = "2/11/2021"
+            });
+
+            listInvoice.Items.Add(new InvoiceDelivery()
+            {
+                No = 2,
+                invoiceID = "124",
+                customerName = "Bao Loc",
+                dueDate = "2/11/2021"
+            });
+
+            listInvoice.Items.Add(new InvoiceDelivery()
+            {
+                No = 3,
+                invoiceID = "125",
+                customerName = "Hon Tuyen",
+                dueDate = "2/11/2021"
+            });
+        }
+    }
+}
---
+++ DigitalPhotographyManagementSystem/UserControls/PrintPhoto.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DigitalPhotographyManagementSystem.UserControls
+{
+    /// <summary>
+    /// Interaction logic for PrintPhoto.xaml
+    /// </summary>
+    /// 
+    public class InvoicePrint
+    {
+        public int No { get; set; }
+        public string invoiceID { get; set; }
+        public string customerName { get; set; }
+        public string Date { get; set; }
+    }
+
+    public partial class PrintPhoto : UserControl
+    {
+        public PrintPhoto()
+        {
+            InitializeComponent();
+
+            List<InvoicePrint> invoices = new List<InvoicePrint>();
+
+            listInvoice.Items.Add(new InvoicePrint()
+            {
+                No = 1,
+                invoiceID = "123",
+                customerName = "Thanh Hien",
+                Date = "2/11/2021"
+            });
+
+            listInvoice.Items.Add(new InvoicePrint()
+            {
+                No = 2,
+                invoiceID = "124",
+                customerName = "Bao Loc",
+                Date = "2/11/2021"
+            });
+
+            listInvoice.Items.Add(new InvoicePrint()
+            {
+                No = 3,
+                invoiceID = "125",
+                customerName = "Hon Tuyen",
+                Date = "2/11/2021"
+            });
+        }
+    }
+}
