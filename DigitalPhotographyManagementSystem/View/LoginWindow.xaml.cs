19f1d5cc44c066dbd0dcd692ee72130035288cc6
baoloc7401@gmail.com <baoloc7401@gmail.com>
2021-11-22 15:31:35 +0700

    Fix login

--- DigitalPhotographyManagementSystem/View/LoginWindow.xaml.cs
+++ DigitalPhotographyManagementSystem/View/LoginWindow.xaml.cs
@@ -22,14 +22,42 @@ namespace DigitalPhotographyManagementSystem.View
         public LoginWindow()
         {
             InitializeComponent();
+            Uname_txt.Focus();
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            this.DragMove();
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void SignInBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //For testing and references
+            //Account_DTO account = new Account_DTO(Uname_txt.Text, Pass_txt.Password);
+            
+            if (/*Account_BUS.Login(account)*/ Uname_txt.Text.ToLower().Equals("admin") && Pass_txt.Password.Equals("12345"))
+            {
+                DashboardMain dashboardMain = new DashboardMain();
+                dashboardMain.Show();
+                this.Close();
+            }
+            else
+            {
+                var messageBoxResult = MsgBox.Show("Warning", "Wrong username or password. Try again!", MessageBoxTyp.Warning);
+
+                Uname_txt.Text = "";
+                Pass_txt.Password = "";
+                Uname_txt.Focus();
+            }
         }
     }
 }
