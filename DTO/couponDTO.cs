63c99b99aa2ea176228fc6b811e50c3415b0c95f
Thanh Hien Tran <71955948+HinTran013@users.noreply.github.com>
2021-11-26 21:04:56 +0700

    add coupon structure

---
+++ DTO/couponDTO.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class couponDTO
+    {
+        private string _couponCode;
+        private string _endDate;
+        private float _saleAmount;
+        private string _invoiceID;
+
+        public couponDTO()
+        {
+            this._couponCode = null;
+            this._endDate = null;
+            this._saleAmount = 0;
+            this._invoiceID = null;
+        }
+
+        public couponDTO(
+            string couponCode,
+            string endDate,
+            float saleAmount,
+            string invoiceID
+        )
+        {
+            this._couponCode = couponCode;
+            this._endDate = endDate;
+            this._saleAmount = saleAmount;
+            this._invoiceID = invoiceID;
+        }
+
+        public string couponCode { get => _couponCode; set => _couponCode = value; }
+        public string endDate { get => _endDate; set => _endDate = value; }
+        public float saleAmount { get => _saleAmount; set => _saleAmount = value; }
+        public string invoiceID { get => _invoiceID; set => invoiceID = value; }
+    }
+}
