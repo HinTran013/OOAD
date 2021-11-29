89f3366fce50f5a76696d3678389182f82cc64dc
baoloc7401@gmail.com <baoloc7401@gmail.com>
2021-11-29 23:48:51 +0700

    Added Basic chart

---
+++ DigitalPhotographyManagementSystem/ServiceClass/RevExpChartViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+
+namespace DigitalPhotographyManagementSystem.ServiceClass
+{
+    class RevExpChartViewModel
+    {
+        public ISeries[] Series { get; set; }
+            = new ISeries[]
+            {
+                new LineSeries<double>
+                {
+                    Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
+                    Fill = null
+                }
+            };
+    }
+}
