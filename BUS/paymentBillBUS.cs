c7b3fd47da3104e9c710b5ea477d4ea96c4d8873
Thanh Hien Tran <71955948+HinTran013@users.noreply.github.com>
2021-11-30 16:20:05 +0700

    add payment bills

---
+++ BUS/paymentBillBUS.cs
@@ -0,0 +1,20 @@
+﻿using DAL;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class paymentBillBUS
+    {
+        static paymentBillDAL paymentBill = new paymentBillDAL();
+
+        public static bool AddNewPaymentBill(paymentBillDTO newPaymentBill)
+        {
+            return paymentBill.InsertNewPaymentBillRecord(newPaymentBill);
+        }
+    }
+}
---
+++ DAL/paymentBillDAL.cs
@@ -0,0 +1,43 @@
+﻿using DTO;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class paymentBillDAL
+    {
+        private IMongoDatabase db;
+
+        public paymentBillDAL()
+        {
+            var client = new MongoClient("mongodb+srv://HienTranOOAD:123123123@cluster0.guxtk.mongodb.net/PhotographyManagement?retryWrites=true&w=majority");
+            this.db = client.GetDatabase("PhotographyManagement");
+        }
+
+        public bool InsertNewPaymentBillRecord(paymentBillDTO newPaymentBill)
+        {
+            try
+            {
+                var collection = db.GetCollection<BsonDocument>("paymentBills");
+                var newDoc = new BsonDocument
+            {
+                { "date" , newPaymentBill.date },
+                {"totalMoney", newPaymentBill.totalMoney },
+                {"accountantID", newPaymentBill.accountantID },
+            };
+
+                collection.InsertOneAsync(newDoc);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
