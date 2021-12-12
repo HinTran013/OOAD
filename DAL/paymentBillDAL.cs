f049460cc04c499e8179c3074a7d113fae443fc5
baoloc7401@gmail.com <baoloc7401@gmail.com>
2021-12-12 10:09:39 +0700

    Fix bug

--- DAL/paymentBillDAL.cs
+++ DAL/paymentBillDAL.cs
@@ -44,7 +44,7 @@ namespace DAL
                     {"staffUsername", newPaymentBill.staffUsername },
                     {"state", newPaymentBill.state },
                     {"date", newPaymentBill.date },
-                    {"invoiceDetail", billDetails},
+                    {"billDetail", billDetails},
                     {"totalMoney", newPaymentBill.totalMoney }
                 };
 
@@ -54,6 +54,43 @@ namespace DAL
             catch
             {
                 return false;
+            }
+        }
+        public paymentBillDTO GetPaymentBillFromID(ObjectId objectId)
+        {
+            try
+            {
+                var collection = db.GetCollection<BsonDocument>("paymentBills");
+                var inv = collection.Find(x => ((ObjectId)x["_id"]) == objectId).SingleAsync().Result;
+                var billdetails = new List<billDetailDTO>();
+                foreach (BsonDocument detail in inv["billDetail"].AsBsonArray)
+                {
+                    billdetails.Add(new billDetailDTO
+                    (
+                        (string)detail["service"],
+                        (int)detail["unitQuantity"],
+                        (int)detail["unitPrice"]
+                    ));
+                }
+                paymentBillDTO paymentBill = new paymentBillDTO
+                (
+                    (string)inv["customerName"],
+                    (string)inv["customerAddress"],
+                    (string)inv["customerPhoneNo"],
+                    (string)inv["customerEmail"],
+                    (string)inv["customerRequestDetail"],
+                    (string)inv["staffUsername"],
+                    (string)inv["state"],
+                    (string)inv["date"],
+                    billdetails,
+                    (ObjectId)inv["_id"]
+                );
+
+                return paymentBill;
+            }
+            catch
+            {
+                return null;
             }
         }
     }
