782d2adb0f634b39a5c1da585828d4b4168c9e1d
Thanh Hien Tran <71955948+HinTran013@users.noreply.github.com>
2021-12-13 10:31:57 +0700

    update photoDAL and photoDTO

---
+++ DAL/photoDAL.cs
@@ -0,0 +1,42 @@
+﻿using DTO;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class photoDAL
+    {
+        private IMongoDatabase db;
+
+        public photoDAL()
+        {
+            var client = new MongoClient("mongodb+srv://HienTranOOAD:123123123@cluster0.guxtk.mongodb.net/PhotographyManagement?retryWrites=true&w=majority");
+            this.db = client.GetDatabase("PhotographyManagement");
+        }
+
+        public bool InsertNewIssueRecord(photoDTO newPhoto)
+        {
+            try
+            {
+                var collection = db.GetCollection<BsonDocument>("photos");
+                var newDoc = new BsonDocument
+            {
+                    { "invoiceID", newPhoto.invoiceID },
+                    { "photoContent", newPhoto.photoContent }
+            };
+
+                collection.InsertOneAsync(newDoc);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
--- DTO/photoDTO.cs
+++ DTO/photoDTO.cs
@@ -8,5 +8,22 @@ namespace DTO
 {
     class photoDTO
     {
+        private string _invoiceID;
+        private byte[] _photoContent;
+
+        public photoDTO()
+        {
+            _invoiceID = null;
+            _photoContent = null;
+        }
+
+        public photoDTO(string invoiceID, byte[] photoContent)
+        {
+            this._invoiceID = invoiceID;
+            this._photoContent = photoContent;
+        }
+
+        public string invoiceID { get => _invoiceID; set => _invoiceID = value; }
+        public byte[] photoContent { get => _photoContent; set => _photoContent = value; }
     }
 }
