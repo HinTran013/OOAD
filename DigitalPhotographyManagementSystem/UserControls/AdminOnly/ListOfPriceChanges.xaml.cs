62de90c520b74ce9112637ed60d4ee0fe0d5ef6a
Thanh Hien Tran <71955948+HinTran013@users.noreply.github.com>
2021-12-19 18:11:44 +0700

    show services

--- DigitalPhotographyManagementSystem/UserControls/AdminOnly/ListOfPriceChanges.xaml.cs
+++ DigitalPhotographyManagementSystem/UserControls/AdminOnly/ListOfPriceChanges.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +14,53 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DTO;
+using BUS;
 
 namespace DigitalPhotographyManagementSystem.UserControls.AdminOnly
 {
     /// <summary>
     /// Interaction logic for ListOfPriceChanges.xaml
     /// </summary>
+    /// 
+    public class services
+    {
+        public int No { get; set; }
+        public ObjectId fullID { get; set; }
+        public string name { get; set; }
+        public double price { get; set; }
+        public string description { get; set; }
+    }
+
     public partial class ListOfPriceChanges : UserControl
     {
+        private ObservableCollection<services> showServices;
+
         public ListOfPriceChanges()
         {
             InitializeComponent();
+
+            List<servicesDTO> services = new List<servicesDTO>();
+            services = servicesBUS.GetAllServices();
+            int i = 1;
+
+            showServices = new ObservableCollection<services>();
+
+            foreach (servicesDTO item in services)
+            {
+                var newShowService = new services()
+                {
+                    No = i++,
+                    name = item.name,
+                    fullID = (ObjectId)item.serviceID,
+                    price = item.price,
+                    description = item.description
+                };
+                showServices.Add(newShowService);
+            }
+
+            listService.ItemsSource = showServices;
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(listService.ItemsSource);
         }
     }
 }
